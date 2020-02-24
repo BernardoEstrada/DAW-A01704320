be57c9ed634344189cffb89101534bbba6c5a950
BernardoEstrada <estradaf.bernardo@gmail.com>
2020-02-24 13:56:54 -0600

    Added Template for product and different colors

--- Lab5/products.js
+++ Lab5/products.js
@@ -1,7 +1,26 @@
 
+class item{
+    name;
+    price;
+    img;
+    Constructor(){
+
+    }
+}
+
 let items = document.getElementById("shop").getElementsByClassName("shopItem");
-console.log(typeof items);
-console.log(items);
+// console.log(typeof items);
+// console.log(items);
 // items.forEach(item=>{
 //     console.log(item.getElementById("price").innerText)
 // });
+
+document.getElementById("hueSl").addEventListener("input",function(){
+    document.getElementsByClassName("hue")[0].style.filter = " brightness(50%) sepia(1) hue-rotate(" + this.value + "deg)";
+});
+
+let rot=0;
+setInterval(()=>{
+    rot=(rot+1)%360;
+}, 10);
+console.log(document.getElementsByClassName("hue")[0].style.filter);
