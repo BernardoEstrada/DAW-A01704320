638c55394a9ea6bc32b8ce3dbe33e00434f7f808
BernardoEstrada <estradaf.bernardo@gmail.com>
2020-02-24 13:54:09 -0600

    Se ha agregado La funcionalidad de verificación de contraseñas

---
+++ Lab5/products.js
@@ -0,0 +1,7 @@
+
+let items = document.getElementById("shop").getElementsByClassName("shopItem");
+console.log(typeof items);
+console.log(items);
+// items.forEach(item=>{
+//     console.log(item.getElementById("price").innerText)
+// });
