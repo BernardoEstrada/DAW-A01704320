77864dcca6deaac8e40a06e1769f3c47e319eab5
BernardoEstrada <estradaf.bernardo@gmail.com>
2020-02-24 21:37:00 -0600

    Added Sign up section

--- Lab5/signup.js
+++ Lab5/signup.js
@@ -1,6 +1,13 @@
+function Person(data){
+    this.name = data.enterName.value;
+    this.email = data.enterEmail.value;
+    this.phone = data.enterPhone.value;
+}
+let people = [];
 document.getElementById("signup").addEventListener("submit", function(event){
-    console.log(this);
     event.preventDefault();
+   people.push(new Person(event.target.elements));
+   table();
 });
 
 document.getElementById("enterName").addEventListener("input", function(){
@@ -8,12 +15,41 @@ document.getElementById("enterName").addEventListener("input", function(){
     try{checkName(this.value); span.innerText = "✔"; span.className = "";}
     catch(e){span.innerText = "❌" + e; span.className = "Error"}
 });
+document.getElementById("enterPhone").addEventListener("input", function(){
+    let span = document.getElementById("phoneValid");
+    try{checkPhone(this.value); span.innerText = "✔"; span.className = "";}
+    catch(e){span.innerText = "❌" + e; span.className = "Error"}
+});
 
 function checkName(nam){
-    let reject = /[\W-]/;
+    let reject = /[^\w- ]/;
     if(reject.test(nam)){
         throw "Invalid Character";
     }
+}
+
+function checkPhone(ph){
+    let reject = /[^0-9]/;
+    if(reject.test(ph)) {
+        throw "Invalid Character";
+    } else if(ph.length!==10){
+        throw "Enter exactly 10 digits";
+    }
+}
+
+function table(){
+    let htmlToAdd = "";
+    htmlToAdd = "<th>Name<th>Email</th><th>Phone</th>";
+    people.forEach(item => {
+        htmlToAdd = htmlToAdd.concat(`
+            <tr>
+                <td>${item.name}</td>
+                <td>${item.email}</td>
+                <td>${item.phone}</td>
+            </tr>
+        `);
+    });
+    document.getElementById("people").innerHTML = htmlToAdd;
 }
 
 //✔
